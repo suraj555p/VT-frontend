800dbaafce878085734334628b7851b84b439e32
suraj <suraj87parmar@gmail.com>
2025-06-29 18:33:12 +0530

    ss

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/v1", // 👈 change if your backend is hosted elsewhere
+  baseURL: "https://vt-backend-iaqk.onrender.com/api/v1", // 👈 change if your backend is hosted elsewhere
   withCredentials: true, // ✅ sends cookies (for auth)
   headers: {
     "Content-Type": "application/json",
