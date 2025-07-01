0ec7d798ac9618ce314c2a06e08202b91bb4f74a
suraj <suraj87parmar@gmail.com>
2025-07-01 12:26:18 +0530

    thet

--- src/components/Video/UploadVideo.jsx
+++ src/components/Video/UploadVideo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from '../../api/axios';
+import axios from '../../api/axios.js';
 
 const UploadVideo = () => {
   const [form, setForm] = useState({
